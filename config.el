c7827129ecc56c3d48af4a9e91f988565b30808d
Axel Paillaud <contact@axelweb.fr>
2025-02-05 05:58:30 +0100

    add ob-js to execute some javascript

--- config.el
+++ config.el
@@ -77,3 +77,10 @@
 ;;
 ;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
 ;; they are implemented.
+
+;; Execute JavaScript
+(require 'ob-js)
+
+(add-to-list 'org-babel-load-languages '(js . t))
+(org-babel-do-load-languages 'org-babel-load-languages org-babel-load-languages)
+(add-to-list 'org-babel-tangle-lang-exts '("js" . "js"))
